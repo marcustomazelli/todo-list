9fd908d606539b770cbe5e541f11a0318388c37f
Marcus Tomazelli <tomazelli@proton.me>
2025-09-28 02:13:19 -0400

    todo list

---
+++ src/clock.js
@@ -0,0 +1,13 @@
+const getHours = () => {
+  const clock = document.querySelector('#clock')
+  const date = new Date()
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  const hour = hours < 10 ? `0${hours}` : hours
+  const minute = minutes < 10 ? `0${minutes}` : minutes
+  const second = seconds < 10 ? `0${seconds}` : seconds
+  clock.innerHTML = `${hour}:${minute}:${second}`
+}
+
+export {getHours};
